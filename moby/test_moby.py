f9159eceaaddc9b6eca134e920ee1d872490a815
Jacob Mossberg <jacob@jacobmossberg.se>
2020-07-19 20:39:18 +0200

    moby commit

--- moby/test_moby.py
+++ moby/test_moby.py
@@ -18,3 +18,19 @@ def test_happy_path():
     moby.run()
     result = "".join(showlines)
     verify(result + "\n")
+
+def test_moby_branch():
+    import os_api
+
+    showlines = []
+
+    def fake_show(showmsg):
+        showlines.append(showmsg)
+
+    os_api.wait = lambda _: True
+    os_api.show = fake_show
+    os_api.git = lambda *args: True
+
+    moby.run()
+    result = "".join(showlines)
+    verify(result + "\n")
