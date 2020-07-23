30c299701a7aba416a22c5998f20966ec7fd74c4
Olof Bjarnson <olof.bjarnason@gmail.com>
2020-07-23 22:29:02 +0200

    reduce test code duplication somewhat

--- moby/test_moby.py
+++ moby/test_moby.py
@@ -1,11 +1,10 @@
 from approvaltests import verify
 
 import moby
+import os_api
 
 
-def test_happy_path():
-    import os_api
-
+def setup_os_api():
     showlines = []
 
     def fake_show(showmsg):
@@ -15,23 +14,18 @@ def test_happy_path():
     os_api.show = fake_show
     os_api.git = lambda *args: "On branch moby-branch"
 
+    return showlines
+
+
+def test_happy_path():
+    showlines = setup_os_api()
     moby.run_once()
     result = "".join(showlines)
     verify(result + "\n")
 
 
 def test_happy_path_run_twice():
-    import os_api
-
-    showlines = []
-
-    def fake_show(showmsg):
-        showlines.append(showmsg)
-
-    os_api.wait = lambda _: True
-    os_api.show = fake_show
-    os_api.git = lambda *args: "On branch moby-branch"
-
+    showlines = setup_os_api()
     moby.run_once()
     moby.run_once()
     result = "".join(showlines)
@@ -39,19 +33,11 @@ def test_happy_path_run_twice():
 
 
 def test_not_on_moby_branch():
-    import os_api
-
-    showlines = []
-
-    def fake_show(showmsg):
-        showlines.append(showmsg)
-
+    showlines = setup_os_api()
     git_status_output = """\
 On branch master
 nothing to commit, working tree clean
 """
-    os_api.wait = lambda _: True
-    os_api.show = fake_show
     os_api.git = lambda *args: git_status_output
 
     moby.run_once()
